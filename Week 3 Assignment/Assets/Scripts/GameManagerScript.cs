96fb90a69ceff535120598cd82f9cdae480df877
Daniel Kimsey <longlegsclassic@gmail.com>
2017-02-13 04:05:31 -0500

    Added GameManager
    gmScript as singleton

--- Week 3 Assignment/Assets/Scripts/GameManagerScript.cs
+++ Week 3 Assignment/Assets/Scripts/GameManagerScript.cs
@@ -3,9 +3,28 @@ using System.Collections;
 
 public class GameManagerScript : MonoBehaviour {
 
+	private int score;
+
+	public int Score{
+		get{
+			return score;
+		}
+		set{
+			score = value;
+
+		}
+	}
+
+	public static GameManagerScript instance;
+
 	// Use this for initialization
 	void Start () {
-	
+		if(instance == null){
+			instance = this;
+		}
+		else{
+			Destroy(this.gameObject);
+		}
 	}
 	
 	// Update is called once per frame
